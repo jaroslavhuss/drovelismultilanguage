a23c11d4bdcb5f30339882a27be7cdc46b2b886f
Jaroslav Huss <jaroslavhuss@HUSS-mac-2.local>
2023-03-06 10:15:06 +0100

    lkajsůdlkjfaůlkj

--- Global_URL.ts
+++ Global_URL.ts
@@ -1,1 +1,3 @@
-export const URL:string = "https://91.139.15.22:10443"
+const globalURL:string = "https://91.139.15.22:10443"
+const localURL:string = "http://localhost:1337"
+export const URL:string = localURL
